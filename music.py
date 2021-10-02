ea0129bc2b95409aa05c844825e34c0c0c81b611
CodedDevil-01 <77150758+CodedDevil-01@users.noreply.github.com>
2021-10-02 12:57:06 +0530

    Add files via upload

---
+++ music.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title('LOKI')
+st.write(
+'''
+theme music !!    
+'''
+)
+
+st.audio('loki_dark_theme.mp3')
+
+st.write()
+
+st.audio('loki_ending_theme_ep2.mp3')
+st.audio('loki_ep_6_title.mp3')
+st.audio('loki_falling.mp3')
+st.audio('loki_god_doesn_t_plead.mp3')
+st.audio('loki_singing.mp3')
+st.audio('loki_theme_v5.mp3')
+st.audio('loki_tva.mp3')
